29cfbd6a41405170c48c86b98b3d46e5f026c2bb
Victor Chiang <vgchiang@gmail.com>
2020-05-24 11:49:41 -0500

    show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+FoodStall.destroy_all
+
+puts "Creating food stall..."
+test_food_stall = { name: 'Orinocos', description: "You've got to try this place.  They have the best tacos in all of Mexico City", food_type: 'tacos', schedule: '24/7', vegetarian: false, address: '123 Avenida de la reforma', phone_number: '1231231231', rating: 5, user: User.last }
+
+first_food_stall = FoodStall.create!(test_food_stall)
+
+puts "created #{first_food_stall}!"
+
