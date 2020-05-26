bbad06cbf2ad4b3de9df5a105b8689df0dad69df
Rodrigo Sanchez <rodrigo.sanchez@linxys.com.mx>
2020-05-26 00:00:22 -0500

    stall add feature

--- app/controllers/food_stalls_controller.rb
+++ app/controllers/food_stalls_controller.rb
@@ -1,4 +1,21 @@
 class FoodStallsController < ApplicationController
-  def food_stall_categories
+
+  def new
+    @food_stall = FoodStall.new
+  end
+
+  def create
+    @food_stall = FoodStall.new(food_stall_params)
+    if @food_stall.save
+      redirect_to food_stall_path(@food_stall)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def food_stall_params
+    params.require(:food_stall).permit(:name, :description, :food_type, :schedule, :vegetarian, :address, :phone_number, :rating, photos: [])
   end
 end
