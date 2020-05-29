d82447e814b901d0dee57b987f7515e25da3c592
TJ Zuvic <terrance@zuvmarketing.com>
2020-05-28 20:42:11 -0400

    Fixed route nesting

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :food_stalls do
+
     collection do
     get :search_results
     end
-
-  end
-  get 'search', to: 'pages#search'
-
 
     member do
     get :menu
     end
 
+   end
 
+    get 'search', to: 'pages#search'
   end
 
-end
