b9ad64790371b55000bb3ab80883557912a356d9
Victor Chiang <vgchiang@gmail.com>
2020-06-24 19:26:58 -0500

    QR code page, click in the hamburger menu

---
+++ app/javascript/packs/qr_code_reader.js
@@ -0,0 +1,21 @@
+import { BrowserQRCodeReader } from '@zxing/library';
+
+const codeReader = new BrowserQRCodeReader();
+
+codeReader
+  .decodeFromInputVideoDevice(undefined, 'video')
+  .then((result) => {
+    // process the result
+    // console.log(result.text)
+
+    // document.getElementById('code-result').textContent = 'SCAN SUCCESSFUL!'
+    document.getElementById('code-result').classList.remove("hidden-success");
+
+     setTimeout(function(){
+       //put your code in here to be delayed by 2 seconds
+       window.location.href = `${result}`;
+     },2000);
+
+    // const sendtopage = window.location.href = `${result}`;
+  })
+  .catch(err => console.error(err));
