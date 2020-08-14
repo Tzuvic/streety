9ddcfc7273e4049f4e0d93436b92df3de9943364
Victor Chiang <vgchiang@gmail.com>
2020-08-13 21:35:13 -0700

    start of cart CSS animation

---
+++ app/javascript/packs/home.js
@@ -0,0 +1,10 @@
+const cart = document.querySelector(".cart");
+console.log(cart);
+
+// cart.addEventListener("DOMContentLoaded", function() {
+//     cart.classList.add("cart-move");
+// });
+
+// cart.addEventListener('click', (event) => {
+//   event.target.classList.add('cart-move');
+// });
